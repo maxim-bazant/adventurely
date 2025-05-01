9d41b7740b81559f01b6948d220182f058038210
maxim-bazant <maxim.bazant@outlook.com>
2025-05-01 21:19:18 +0200

    removed add button and moved the add card to be the first one

--- src/pages/Trips/TripsPage.tsx
+++ src/pages/Trips/TripsPage.tsx
@@ -1,7 +1,6 @@
 import styles from './Trips.module.scss';
 import Card from '../../components/Card/Card';
 import DashboardHeader from '../../components/DashboardHeader/DashboardHeader';
-import { useNavigate } from 'react-router-dom';
 import useTripStore from '../../store/trip';
 import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -11,24 +10,19 @@ import CardGrid from '../../components/CardGrid/CardGrid';
 
 const TripsPage = () => {
     const { user } = useAuth0();
-    const navigate = useNavigate();
     const { fetchTripsForUser, trips, isLoading } = useTripStore();
 
     useEffect(() => {
         fetchTripsForUser(user!.sub!);
     }, [fetchTripsForUser, user]);
 
-
-    const handleAddTrip = () => {
-        navigate('/my-trips/add');
-    }
-
     return (
         ( isLoading && <Loader message="Loading your trips..."/> )||
         <div className={styles.container}>
-            <DashboardHeader title="My Trips" button={{ title: "+ Add Trip", onClick: handleAddTrip }}/>
+            <DashboardHeader title="My Trips"/>
             
             <CardGrid>
+                <Card addCard={true} />
                 {/* Reverse the trips array to show the latest trip first */}
                 {trips.reverse().map((trip) => (
                     <Card
@@ -36,7 +30,6 @@ const TripsPage = () => {
                         trip={trip}
                     />
                 ))}
-                <Card addCard={true} />
             </CardGrid>
             </div>
     );
